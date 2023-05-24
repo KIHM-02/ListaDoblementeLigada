5e6587fa277341c4da97e503604d482d7e40f5a4
KIHM-02 <monteroisaac33@gmail.com>
2023-05-23 20:24:49 -0600

    First commit

---
+++ src/listadobleligada/Main.java
@@ -0,0 +1,14 @@
+package listadobleligada;
+
+/**
+ *
+ * @author Kevin Huerta Montero 22310411 2P
+ */
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
